dab430dc24213aa1b4f3a7bcc64f9f23a58e9328
student <student@192.168.10.106>
2024-05-20 11:49:44 +0900

    test push

---
+++ src/main/java/com/example/demo/PersonalDev7530ShioyaHayatoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev7530ShioyaHayatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev7530ShioyaHayatoApplication.class, args);
+	}
+
+}
